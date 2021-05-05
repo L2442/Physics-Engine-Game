a62ebfc7ce4db9f8eae2a3b28613180e43ddb62f
L2442 <76606422+L2442@users.noreply.github.com>
2021-05-05 18:25:32 +0530

    Add files via upload

--- ball.js
+++ ball.js
@@ -9,7 +9,7 @@ class Ball{
       this.visibility = 255;
       this.width = width;
       this.height = height;
-      this.body = Bodies.circle(x,y,width,height,options);
+      this.body = Bodies.rectangle(x,y,width,height,options);
       World.add(world,this.body);
   }
   display(){
